3f03870514fe1beb84c3d66d5ddeb8c00e089840
seintun <seintun.edu@gmail.com>
2018-10-22 20:34:52 -0700

    added styling on background, carosule arrow color, images for each menu items

--- templates/items.template.js
+++ templates/items.template.js
@@ -7,12 +7,36 @@ const templateItems = (items) => {
 }
 
 const templateItem = (item) => {
-    return `<li id= "${item.id}" class="list-group-item m-1">
-                <h4>${item.name}</h4>
-                <small>${item.descriptions}</small>
-                <br />
-                <strong>$${item.price}</strong>
-                <br />
-                <em>Allergens: ${item.allergens}</em>
-            </li>`;
+    return `
+        <div class="row">
+            <div class="col-3 m-1">
+                <img src="https://source.unsplash.com/200x20${item.id}/?food" alt="">
+            </div>
+            <div class="col-8 float-right">
+                <li id= "${item.id}" class="list-group-item m-1">
+                    <h4>${item.name}</h4>
+                    <small>${item.descriptions}</small>
+                    <br />
+                    <strong>$${item.price}</strong>
+                    <br />
+                    <em>Allergens: ${item.allergens}</em>
+                </li>
+            </div>
+        </div>`;
 }
+
+// <div class="row">
+// <div class="col-3">
+//     <img src="https://source.unsplash.com/200x200/?food" alt="">
+// </div>
+// <div class="col-9 float-right">
+//     <li id= "${item.id}" class="list-group-item m-1">
+//         <h4>Black Cod</h4>
+//         <small>Saskatchewan chanterelles, sunflower seed fat, spinach</small>
+//         <br />
+//         <strong>$27</strong>
+//         <br />
+//         <em>Allergens: Seafood</em>
+//     </li>
+// </div>
+// </div>
